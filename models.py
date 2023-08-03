bdefa8f957ef3b4915221b3753423b65ba73ff5d
MashaK28 <137155231+MashaK28@users.noreply.github.com>
2023-08-03 19:18:37 +0500

    Add files via upload

---
+++ models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class Advertisement(models.Model):
+    title = models.CharField('Заголовок', max_length=128)
+    description = models.TextField('Описание')
+    price = models.DecimalField('Цена', max_digits=10, decimal_places=2)
+    auction = models.BooleanField('Торг', help_text='Отметьте, если торг уместен')
+    created_at = models.DateTimeField('Дата и время создания', auto_now_add=True)
+    updated_at = models.DateTimeField('Дата и время изменения', auto_now=True)
+
+
+    class Name:
+        db_table = "advertisement"
+
+
+    def __str__(self):
+        return f"Advertisement(id={self.id}, title={self.title}, price={self.price})"
