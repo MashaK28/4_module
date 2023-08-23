67745f2739887dda4b9b6f66f4508e1c0bff52a4
MashaK28 <137155231+MashaK28@users.noreply.github.com>
2023-08-23 20:58:39 +0500

    Add files via upload

--- apps.py
+++ apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
 
 
-class AppLesson4Config(AppConfig):
+class AppAdvertismentConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'app_lesson_4'
+    name = 'app_advertisment'
+    verbose_name = 'Объявления'
--- models.py
+++ models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib import admin
+from django.utils.html import format_html
+from django.utils import timezone
 
 
 class Advertisement(models.Model):
@@ -9,6 +12,23 @@ class Advertisement(models.Model):
     created_at = models.DateTimeField('Дата и время создания', auto_now_add=True)
     updated_at = models.DateTimeField('Дата и время изменения', auto_now=True)
 
+    @admin.display(description='Дата создания')
+    def created_date(self):
+        if self.created_at.date() == timezone.now().date():
+            created_time = self.created_at.time().strftime('%H:%M:%S')
+            return format_html(
+                '<span style="color:green; font-weight:bold;"> Сегодня в {}</span>', created_time
+            )
+            return self.created_at.strftime('%d.%m.%Y в %H:%M:%S')
+
+    @admin.display(description='Дата обновления')
+    def updated_date(self):
+        if self.created_at.date() == timezone.now().date():
+            created_time = self.updated_at.time().strftime('%H:%M:%S')
+            return format_html(
+                '<span style="color:red; font-weight:bold;"> Сегодня в {}</span>', created_time
+            )
+            return self.created_at.strftime('%d.%m.%Y в %H:%M:%S')
 
     class Name:
         db_table = "advertisement"
