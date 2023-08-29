299a10efaf2f8534776ed6d5c6ce6ea54e5edfa7
MashaK28 <137155231+MashaK28@users.noreply.github.com>
2023-08-29 14:19:28 +0500

    Add files via upload

--- forms.py
+++ forms.py
@@ -15,7 +15,7 @@ class AdvertisementForm(forms.ModelForm):
             'image':forms.FileInput(attrs={'class': 'form-control form-control-lg'}),
         }
 
-    def clean_title(self):
+    def mistake(self):
         title = self.cleaned_data['title']
         if title.startswith('?'):
             raise ValidationError('Заголовок не может начинаться с вопросительного знака')
